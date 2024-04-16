a8839c3c9d9c00df451ce35d3a2215b9ac6b9f43
jumashafara <jumashafara0@gmail.com>
2024-04-16 09:46:43 +0300

    added files

---
+++ betty_kawala/practice/lists.py
@@ -0,0 +1,12 @@
+# list of ages
+# ages = [12, 14, 16, 18, 20, 22]
+# mean = sum(ages) / len(ages)
+# print(mean)
+
+pets = ['dog', 'cat', 'rabbit', 'horse', 'parrot', 'lion']
+pets_to_remove = ['lion', 'horse']
+
+for pet in pets_to_remove:
+    pets.remove(pet)
+
+print(pets)
